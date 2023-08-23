4be38e3a680052d286064a9e09438f6e2d7bd4d7
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2023-08-23 15:31:29 -0500

    Se actualiza Dockerfile

--- users/src/blueprints/resources.py
+++ users/src/blueprints/resources.py
@@ -9,33 +9,39 @@ from utilities.utilities import formatDateTimeToUTC
 
 users_blueprint = Blueprint('users', __name__)
 
+# Recurso que expone la funcionalidad healthcheck
 @users_blueprint.route('/users/ping', methods=['GET'])
 def health():
     return "pong"
 
+# Recurso que expone la funcionalidad reset users
 @users_blueprint.route('/users/reset', methods=['POST'])
 def reset():
     ResetUsers().execute()
     return jsonify({'msg': 'Todos los datos fueron eliminados'})
 
+# Recurso que expone la funcionalidad authenticate
 @users_blueprint.route('/users/auth', methods=['POST'])
 def authenticate():
     data = request.get_json()
     result = Authenticate(data).execute()
     return jsonify({'id': result.id, 'token': result.token, 'expireAt': formatDateTimeToUTC(str(result.createdAt))})
 
+# Recurso que expone la funcionalidad detail user
 @users_blueprint.route('/users/me', methods=['GET'])
 def detail():
     data = request.headers
     result = GetUserDetail(data).query()
     return jsonify({'id': result.id, 'username': result.username, 'email': result.email, 'fullName': result.fullName, 'dni': result.dni, 'phoneNumber': result.phoneNumber, 'status': result.status})
 
+# Recurso que expone la funcionalidad update user
 @users_blueprint.route('/users/<string:userId>', methods=['PATCH'])
 def update(userId):
     data = request.get_json()
     UpdateUser(userId, data).execute()
     return jsonify({'msg': 'el usuario ha sido actualizado'})
 
+# Recurso que expone la funcionalidad create user
 @users_blueprint.route('/users', methods=['POST'])
 def create():
     data = request.get_json()
--- users/src/commands/authenticate.py
+++ users/src/commands/authenticate.py
@@ -11,7 +11,7 @@ import uuid
 import os
 
 # Constantes
-TOKEN_DURATION_MIN =  os.environ["TOKEN_DURATION_MIN"]
+TOKEN_DURATION_MIN =  os.getenv("TOKEN_DURATION_MIN", default=10)
 # Clase que contiene la logica de creción de usuarios
 class Authenticate(BaseCommannd):
     def __init__(self, user):
