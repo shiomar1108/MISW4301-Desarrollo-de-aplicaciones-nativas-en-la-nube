430839219b6b7028f2da285a62127f346628d35e
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-08-23 19:48:15 -0600

    Se corrige errores de Post

--- posts/src/blueprints/api.py
+++ posts/src/blueprints/api.py
@@ -7,6 +7,5 @@ api_blueprint = Blueprint('vistas', __name__)
 api = Api(api_blueprint)
 api.add_resource(VistaPosts, '/posts')
 api.add_resource(VistaPostsReset, '/posts/reset')
-api.add_resource(VistaPost, '/post/<string:postId>')
 api.add_resource(VistaPostHealthCheck, '/posts/ping')
-    
+api.add_resource(VistaPost, '/posts/<string:postId>')
