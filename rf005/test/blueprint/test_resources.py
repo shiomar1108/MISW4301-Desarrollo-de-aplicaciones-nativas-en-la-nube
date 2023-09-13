dba9bffe5169fd662aa1979e83f70a84efe2e770
Jhon Guzman <jfguzman@indracompany.com>
2023-09-12 21:08:03 -0500

    se ajusta test succes

--- rf005/test/blueprint/test_resources.py
+++ rf005/test/blueprint/test_resources.py
@@ -3,14 +3,11 @@ from faker import Faker
 from datetime import datetime, timedelta
 
 from tests.mocks import (
-    mock_failed_auth,
-    mock_success_auth,
-    mock_forbidden_auth,
-    mock_query_route_empty,
-    mock_user_post_route_success,
-    mock_post_route_error,
-    mock_post_route_success,
-    mock_post_create_success,
+    mmock_post_route_success,
+    mock_post_posts_success,
+    mock_post_users_success,
+    mock_post_offers_success,
+    mock_post_scores_success
 )
 from faker.providers import DynamicProvider
 from httmock import HTTMock
